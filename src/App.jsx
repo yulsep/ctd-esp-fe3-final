cd7c882a3e406d3f4c56eb3b72a82853a3577cea
yuliana <yuliana.sepulveda@alternova.com>
2024-09-09 22:37:37 -0500

    feat: create routes for each view

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-
+import { routes } from "./Routes";
+import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
+import { useGlobalContext } from "./Context/Context";
 
 function App() {
+  const { theme } = useGlobalContext();
+
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
+    <div className={`App ${theme}`}>
+      <Navbar />
+      <div className="content">
+        <Routes>
+          <Route path={routes.home} element={<Home />} />
+          <Route path={routes.contact} element={<Contact />} />
+          <Route path={routes.detail} element={<Detail />} />
+          <Route path={routes.favs} element={<Favs />} />
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
       </div>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/Routes.js
@@ -0,0 +1,6 @@
+export const routes = {
+  home: "/",
+  contact: "/contact",
+  detail: "/dentist/:id",
+  favs: "/favs",
+};
