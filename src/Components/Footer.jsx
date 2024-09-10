4a664306e240e1403d9346854603cefc530efe6a
yuliana <yuliana.sepulveda@alternova.com>
2024-09-09 22:38:12 -0500

    feat: create footer and navbar

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-
 const Footer = () => {
   return (
     <footer>
-        <p>Powered by</p>
-        <img src="./img/DH.png" alt='DH-logo' />
+      <p>Powered by</p>
+      <img src="../../public/images/DH.png" alt="DH-logo" />
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,67 @@
-import React from 'react'
-
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+import { Link } from "react-router-dom";
+import { routes } from "../Routes";
+import { useGlobalContext } from "../Context/Context";
 
 const Navbar = () => {
+  const { theme, dispatch, setTheme } = useGlobalContext();
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    dispatch({ type: "changeTheme", payload: newTheme });
+  };
 
   return (
     <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+      <div className={`nav-links ${theme}`}>
+        <Link to={routes.home}>HOME</Link>
+        <Link to={routes.contact}>CONTACT</Link>
+        <Link to={routes.favs}>FAVORITES</Link>
+      </div>
+      <button className={`theme-btn ${theme}`} onClick={toggleTheme}>
+        <p>Change theme</p>
+        {theme === "light" ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <circle cx="12" cy="12" r="10" />
+            <path d="M12 2a7 7 0 1 0 10 10" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-sun"
+          >
+            <circle cx="12" cy="12" r="4" />
+            <path d="M12 2v2" />
+            <path d="M12 20v2" />
+            <path d="m4.93 4.93 1.41 1.41" />
+            <path d="m17.66 17.66 1.41 1.41" />
+            <path d="M2 12h2" />
+            <path d="M20 12h2" />
+            <path d="m6.34 17.66-1.41 1.41" />
+            <path d="m19.07 4.93-1.41 1.41" />
+          </svg>
+        )}
+      </button>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
